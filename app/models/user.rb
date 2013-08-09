3747c36b4f0d2c0149b538b8c78eee249494f04d
Grant Birchmeier <gbirchmeier@connamara.com>
2013-08-09 13:49:40 -0500

    no more default scopes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,6 @@
 class User < ActiveRecord::Base
   has_many :posts, :inverse_of=>:user
 
-#  attr_accessible :username,:email
-#  attr_accessible :password, :password_confirmation
-
-  default_scope { order(:username) }
-
   has_secure_password
   validates_presence_of :password, :on => :create
 
