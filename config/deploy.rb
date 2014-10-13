1ada1b1e691483a997926343b353342889dbcfc2
Grant Birchmeier <gbirchmeier@connamara.com>
2014-10-13 15:15:59 -0500

    get rails console to start

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,18 +38,35 @@ namespace :deploy do
 
   before 'deploy:updated', 'custom:symlink_db_config'
 
+
+  desc 'bug demo'
+  task :bugdemo do
+    on roles(:app), in: :sequence, wait: 5 do
+      within release_path do
+        execute "cd #{fetch(:deploy_to)}/current && ls Gemfile"  #kludge
+
+        execute 'pwd'         # behaves as expected
+        execute 'ls'          # See Gemfile in listing
+        execute 'ls Gemfile'  # "No such file..." BUT YOU JUST SAW IT!
+      end
+    end
+  end
+
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       pid_file = fetch(:pid_file)
-      pid = File.read(pid_file).to_i
-      Process.kill 9, pid
-      File.delete pid_file
 
-      execute 'bundle exec rails server' +
-        " -p #{fetch(:server_port)}" +
-        " -d" +
-        " --pid=#{pid_file} "
+      # yes, this sucks.  Will switch to puma or something in the near future.
+      within release_path do
+        execute "((ls #{pid_file} && ps -p `cat #{pid_file}`) && kill -9 `cat #{pid_file}`) || true"
+        execute "(ls #{pid_file} && /bin/rm #{pid_file}) || true"
+        execute "cd #{fetch(:deploy_to)}/current && bundle exec rails server" +
+          " -e #{fetch(:rails_env)}" +
+          " -p #{fetch(:server_port)}" +
+          " -d" +
+          " --pid=#{pid_file} "
+      end
     end
   end
 
