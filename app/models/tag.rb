84ad694af7a2972a2818e32add8149df88de91bc
Grant Birchmeier <gbirchmeier@connamara.com>
2014-10-15 15:13:24 -0500

    forgot in previous commit

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,9 +2,12 @@ class Tag < ActiveRecord::Base
   has_many :post_tags
   has_many :posts, :through => :post_tags
 
+  validates_format_of :nice_url, with: /\A\s*[0-9a-zA-Z\-_]+\s*\z/, allow_blank: true
+
   TAG_SEPARATOR = " "
 
-  def pretty_path
+  def path
     "/tags/#{self.name}"
   end
+
 end
