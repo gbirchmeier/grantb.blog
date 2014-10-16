b40e63229e9d911f5991c654393410a0db6fa9f3
Grant Birchmeier <gbirchmeier@connamara.com>
2014-10-16 08:20:44 -0500

    show *newest* post first.  boy, that was a boner.

--- app/controllers/front_page_controller.rb
+++ app/controllers/front_page_controller.rb
@@ -3,7 +3,7 @@ class FrontPageController < ApplicationController
   FRONT_PAGE_POSTS_COUNT = 1
 
   def index
-    @posts = Post.published.order("published_at DESC").reverse_order.limit(FRONT_PAGE_POSTS_COUNT+1)
+    @posts = Post.published.order("published_at DESC").limit(FRONT_PAGE_POSTS_COUNT+1)
     @prev_post = @posts.pop if @posts.count > 1
   end
 
