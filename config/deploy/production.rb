75e0e1e33217e4083070abfca515eb304676c09a
Grant Birchmeier <gbirchmeier@connamara.com>
2014-10-22 13:05:25 -0500

    change location of deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,7 @@ role :web, %w{172.245.32.193}
 role :app, %w{172.245.32.193}
 role :db,  %w{172.245.32.193}
 
-set :deploy_to, '/home/blog/production'
+set :deploy_to, '/home/blog/rails_apps/blog/prod'
 set :branch, 'deploy-prod'
 set :pid_file, '/home/blog/pids/grantb.blog.prod.pid'
 set :server_port, 3222
